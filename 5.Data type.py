73f19d65549bb8076562d136a3971ab065540bcd
Pratik Choubey <85600312+pratik-choubey@users.noreply.github.com>
2024-05-30 23:33:37 +0530

    DataTypes

---
+++ 5.Data type.py
@@ -0,0 +1,33 @@
+#data type
+'''1.None,2.Numric{int,float,complex,bool},3.List,4.Tuple,5.Set,6.Strig,7.Range
+,8.Dictionary'''
+i=2
+print(type(i))
+i=0+0j
+print(type(i))
+i=2.0
+print(type(i))
+b=int(i)
+print(type(b))
+i= float(b)
+print(type(i))
+e=4
+i=7
+c = complex(e,i)
+print(c)
+b=e<i
+print(b)
+print(type(b))
+print(int(True))
+print(range(10))
+b=list(range(10))
+print(type(b))
+print(b)
+b=list(range(2,10,2))
+print(b)
+l='pratik'
+print(l[3])
+print(len(l))
+#Type casting
+print(int(2.333))
+print(float(2))
