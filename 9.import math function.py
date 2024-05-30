b8243af64199086021734ef727b0e3998e468a1d
Pratik Choubey <85600312+pratik-choubey@users.noreply.github.com>
2024-05-30 23:43:26 +0530

    MathLibrary

---
+++ 9.import math function.py
@@ -0,0 +1,18 @@
+#math function
+import math
+x=math.sqrt(25)
+print(x)
+x=math.sqrt(15)
+print(x)
+print(math.ceil(2.9))
+print(math.floor(2.9))
+print(math.pow(3,4))
+print(math.pi)
+print(math.e)
+#import math as m
+import math as m
+print(m.sqrt(25))
+from math import pow, sqrt
+print(pow(4,5))
+print(sqrt(25))
+
