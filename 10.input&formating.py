d2d293413dd8b0232ad189ba8f267a14b6fd70f5
Pratik Choubey <85600312+pratik-choubey@users.noreply.github.com>
2024-05-30 23:44:20 +0530

    Input

---
+++ 10.input&formating.py
@@ -0,0 +1,11 @@
+#take input and find cube
+x=int(input('Enter a number'))
+y=x**3
+print(y)
+result=eval(input("Enter an exp "))
+print(result)
+#formating
+x=6
+y=7
+z=x+y
+print("sum of {} and {} is {}".format(x,y,z))
