300658c740d86d80e5975cc0e97f668c0b83fde8
Pratik Choubey <85600312+pratik-choubey@users.noreply.github.com>
2024-05-30 23:45:00 +0530

    ifelse

---
+++ 11.if elif else statement.py
@@ -0,0 +1,25 @@
+#suit#indentation
+x=9
+if x==9:
+   print("true")
+if True:
+   print("hi")
+if False:
+   print("bye")
+#even odd
+x = float(input("enter a number"))
+r = x%2
+if r==0:
+   print("even")
+else:
+   print("odd")
+#elif
+x=2
+if x==1:
+   print("One")
+elif x==2:
+   print("Two")
+elif x==3:
+   print("three")
+else:
+   print("Enter appropriat nunber")
