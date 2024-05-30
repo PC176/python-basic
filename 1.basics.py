d6bd53543c7bc69937d20a8609017d2988f21beb
Pratik Choubey <85600312+pratik-choubey@users.noreply.github.com>
2024-05-30 22:56:36 +0530

    Intro

---
+++ 1.basics.py
@@ -0,0 +1,65 @@
+#DEVELOPER-guido van rossum at CWI in 1995
+#this is comment
+#2nd line
+'''we can do multiline comment
+Like this'''
+
+
+#Basics
+print('pratik{}')
+str1="Hello"
+print(str1)
+#for new line
+str2="""you are 
+Good"""
+print(str2)
+#print("what's up") print(str1) this is wrong
+print("what's up");print(str1) 
+print("\n")
+#numerical variable
+var1=12
+var2=38.99
+print(var1)
+print(var2)
+#we can assign more then one variable at a time lie below-
+var1=var2=var3=1000
+print(var1)
+print(var2)
+print(var3)
+#to give different values
+var1,var2,var3=19,34355.88,"string"
+print(var1)
+print(var2)
+print(var3)
+print("\n")
+# var1  //call in shell only
+print(str1[0])
+print(str1[0:])
+print(str1[0:3])
+print(str2[3:5])
+#concatenation
+print("\n")
+print(str1+"world")
+#arethmetic solution
+print("\n")
+var=4
+print(8+2*4)  #<-- 
+print((8+2)*4)
+print(var1+var)
+#Python-data type
+#numeric types (int, float, complex),
+#string (str), boolean (bool), and collection types (list, tuple, dict, set)
+#1.integer,2.float,3.complex number
+print("\n")
+var4=5+6j
+var5=4+5j
+print(var4+var5)
+del var5
+#Raw string
+print("\n")
+print('c:\docs\new')
+print(r'c:\docs\new')
+
+
+
+
