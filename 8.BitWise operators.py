bbceda2216db6e61e79fc31004e4bf354d632738
Pratik Choubey <85600312+pratik-choubey@users.noreply.github.com>
2024-05-30 23:40:35 +0530

    BitwiseOperators

---
+++ 8.BitWise operators.py
@@ -0,0 +1,15 @@
+#bitwise operaotrs
+#1.Complement(~)(2nd complement)
+print(~12)
+print(~0)
+print(~1)
+#2.Bitwise OR, 3.Bitwise and
+print(12&13)
+print(12|13)
+#4.bitwise XOR
+print(12^13)
+print(12^1)
+#5.Left sift
+print(10<<2)
+#6.Right sift
+print(10>>2)
