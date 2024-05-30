bac210e9041a4179c78fe283e25522bfafd53672
Pratik Choubey <85600312+pratik-choubey@users.noreply.github.com>
2024-05-30 23:37:46 +0530

    Operators

---
+++ 6.Operators.py
@@ -0,0 +1,37 @@
+# Operators
+# 1.Arithmetic Operators
+# 2.Assingment operators
+x = 5
+y = 7
+z = x+y
+print(z)
+z += 2
+print(z)
+z -= 3
+print(z)
+z = x*y
+print(z)
+z *= 2
+print(z)
+# 3.Relational operators
+# 4.Logical operators
+print(x < y)
+print(x > y)
+print(x == y)
+x = y
+print(x == y)
+print(x > y and x < y)
+print(x > y and x == y)
+print(x > y or x == y)
+print(y != x)
+# 5.Unary operators
+n = 7
+print(n)
+print(-n)
+n = -n
+print(n)
+#Q
+a = 15
+b = 12
+x = (a//4+b**3) < 2000 and (b % 4 != 0)
+print(x)
